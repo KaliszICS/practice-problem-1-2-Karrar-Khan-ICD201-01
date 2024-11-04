b448964b27f07b1c7dd8f0209e39075c62f16aa1
Karrar-Khan-ICD201-01 <Karrar.khan@student.tdsb.on.ca>
2024-11-04 10:04:27 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -3,18 +3,43 @@
 #Write Assignment code here
 
 def q1():
-
+name = "Alex"
+print (name)
 def q2():
+three = 3
+five = 5
+two = 2
+
+print (three + five + two)
 
 def q3():
+int1 = 21
+int2 = 4 
+
+print (int1//int2)
 
 def q4():
+6.3 == float()
+3.0 == float
+print (6.3/3.0)
 
 def q5():
+salutations = "Hello World"
+print (salutations)
+salutations = "Goodbye World"
+print (salutations)
 
 def q6():
+correct = "true"
+
+print (correct)
 
 def q7():
+int1 = 7
+int2 = 2
+
+print (int1%int2)
+
 
 q1()
 q2()
